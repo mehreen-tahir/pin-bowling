a6b2dc506c2b93d8684558e883776a8addbf7914
Mehreen <mehreentahir18@gmail.com>
2020-03-04 01:30:06 +0200

    Added model helper methods for game

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,2 +1,19 @@
 class Frame < ApplicationRecord
+  MAX_PINS = 10
+
+  scope :ordered_by_number, -> { order(number: :asc) }
+  belongs_to :game
+
+  def strike?
+    first_roll.to_i.eql?(MAX_PINS) && second_roll.blank?
+  end
+
+  def spare?
+    pins_knocked = first_roll.to_i + second_roll.to_i
+    pins_knocked.eql?(MAX_PINS) && first_roll.present? && second_roll.present?
+  end
+
+  def deserves_bonus?
+    strike? || spare?
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,28 @@
 class Game < ApplicationRecord
+  TOTAL_FRAMES = 10
+
+  scope :ordered_by_time, -> { order(created_at: :asc) }
+  has_many :frames, dependent: :destroy
+
+  def open_frame
+    self.frames.ordered_by_number.last
+  end
+
+  def prev_frame
+    open_frame_number = open_frame.number
+    open_frame_number.eql?(1) ? nil : self.frames.where(number: open_frame_number - 1).first
+  end
+
+  def verify_input(input)
+    return [false, 'Input is invalid'] if (open_frame.first_roll.to_i + input > Frame::MAX_PINS) || input < 0
+    return [false, 'Game over, Max Frame limit is reached'] if frames_limit_reached?
+
+    #TODO handle bonus round balls
+    true
+  end
+
+  def frames_limit_reached?
+    return false if open_frame.number < TOTAL_FRAMES
+    open_frame.number >= TOTAL_FRAMES && open_frame.second_roll.present?
+  end
 end
